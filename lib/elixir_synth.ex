faa74ebe046cc67a672086e09dd3ddb708cc358a
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-07 11:53:36 +0900

    コメント

--- lib/elixir_synth.ex
+++ lib/elixir_synth.ex
@@ -7,9 +7,14 @@ defmodule ElixirSynth do
   @snare 38
 
   def hello do
+    # MIDI 初期化
     {:ok, synth} = MIDISynth.start_link([])
+
+    # 最初だけ同期が取れなかったから、ダミーで1音ならす　理由はわからない
+    note(@snare, synth, 1000, 0)
     Process.sleep(100)
 
+    # 8小節最初
     Enum.each(1..8, fn _ ->
       play(synth)
     end)
@@ -18,16 +23,28 @@ defmodule ElixirSynth do
   end
 
   def play(synth) do
+    #ハイハット 8分刻み
     hi_task = notes(@hi, 100, 8, synth)
+
+    #バスドラ 4分刻み
     bass_drum_task = notes(@bass_drum, 400, 4, synth)
+
+    #スネア 裏拍
     snare_task = notes(@snare, 400, 2, synth, 600)
 
+    # 小節終わるまで待機
     Task.await(hi_task)
     Task.await(bass_drum_task)
     Task.await(snare_task)
   end
 
   def notes(note, time, conut, synth, before \\ 0) do
+    # 指定した回数音を鳴らす
+    # note　ノートナンバー
+    # time 間隔
+    # 鳴らす回数
+    # synth　MIDIオープンした変数
+    # 事前のスリーブ時間
     Task.async(fn ->
       Enum.each(1..conut, fn _ ->
         note(note, synth, time, before)
